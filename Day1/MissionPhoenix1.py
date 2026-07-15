3615d0066fc4ac267d96bba2720f54261049726c
KHemanth-18 <jfrnnvr59n@privaterelay.appleid.com>
2026-07-15 22:26:32 +0530

    Add files via upload

---
+++ Day1/MissionPhoenix1.py
@@ -0,0 +1,49 @@
+#Mission Phoenix
+
+print("Hi Phoenix, finally you're doing it!")
+
+#VARIABLES
+##STRINGS
+
+company_name = input("Enter the company name: ")
+experience_years = input("Enter your experience in years: ")
+designation = input("Enter your designation: ")
+
+print(f"I'm working at {company_name}")
+print(f"I have {experience_years} years of experience")
+print(f"My designation is {designation}")
+
+#Integers
+
+year_of_joining = 2024
+salary_lpa = 4
+print(f"I joined the company in {year_of_joining}")
+print(f"My salary offered is {salary_lpa} LPA")
+
+#Floats
+
+total_working_hours = 7.5
+distance_from_home_to_office = 2.71
+monthly_transport_expense = 1400.60
+
+print(f"My total working hours are {total_working_hours} hours")
+print(f"The distance from home to office is: {distance_from_home_to_office} km")
+print(f"My monthly transport expense is: {monthly_transport_expense} INR")
+
+#Booleans
+
+startup_company = False
+us_based_company = True
+
+print(f"Is the company a startup? {startup_company}")
+print(f"Is the company us-based? {us_based_company}")
+
+if startup_company:
+    print("Yes, the company is a startup.")
+else:
+    print("No, the company is not a startup.")
+
+if us_based_company:
+    print("Yes, the company is us-based.")
+else:
+    print("No, the company is not us-based.")
